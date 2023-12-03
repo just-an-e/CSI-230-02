8d2be3d76a600bf7da143c31788bcd3c75313a22
just-an-e <48094071+just-an-e@users.noreply.github.com>
2023-12-03 04:26:26 -0500

    Add files via upload

---
+++ week9/calculator.ps1
@@ -0,0 +1,6 @@
+﻿#3. Write a program that can start and stop the Windows Calculator only using Powershell and using only the process name for the Windows Calculator (to start and stop it).
+
+# Open and close calculator
+Start-Process -FilePath "C:Windows\System32\calc.exe"
+sleep 2
+Stop-Process -Name win32calc
